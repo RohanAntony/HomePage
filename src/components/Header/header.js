398c6f2c11c37c86e60f70ed2c732e583345e77c
Rohan Antony <rohan271995@gmail.com>
2019-06-30 18:19:15 +0530

    Integrated with react

---
+++ src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function header(props){
+	return (
+		<header class="navbar">
+			<h1 class="site-name">rantony</h1>
+			<a href="https://blog.rantony.com" class="link">Blog</a>
+		</header>
+	)
+}
+
+export default header;
