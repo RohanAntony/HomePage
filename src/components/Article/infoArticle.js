a89a0c9292dd3e1ab2df33a483ac784472289c47
Rohan Antony <rohan271995@gmail.com>
2019-07-01 19:29:25 +0530

    Added actively looking for opportunities

--- src/components/Article/infoArticle.js
+++ src/components/Article/infoArticle.js
@@ -7,6 +7,7 @@ function InfoArticle(props){
 			<img src="https://avatars3.githubusercontent.com/u/5779016?s=460&v=4" alt="Profile"/>
 			<div class="about">
 				<p class="name">Rohan Antony</p>
+				<span class="career"><i class="fas fa-circle icon"></i> &nbsp;Actively looking for oppurtunities</span>
 				<hr/>
 				<p>A little bit of that, a little bit of this and somehow everything. </p>
 				<p>Interested in web development, mainly full stack development with React and Django. </p>
@@ -14,8 +15,8 @@ function InfoArticle(props){
 				<p>Also interested in Machine Learning especially supervised machine learning.</p>
 			</div>
 			<div class="links">
-				<a href="https://github.com/RohanAntony">Github</a>
-				<a href="https://www.linkedin.com/in/rohanantony/">LinkedIn</a>
+				<p><i class="fab fa-github icon"></i><a href="https://github.com/RohanAntony">Github</a></p>
+				<p><i class="fab fa-linkedin icon"></i><a href="https://www.linkedin.com/in/rohanantony/">LinkedIn</a></p>
 			</div>
 		</article>
 	)
