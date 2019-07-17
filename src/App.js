f583faa4fc0dd70720ab946f00d49fcd564d335d
Rohan Antony <rohan271995@gmail.com>
2019-07-17 19:21:25 +0530

    Converted App to a component which will be used to handle states of every object

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Nav from './components/Navbar/nav';
 import Section from './components/Section/section';
 import Overlay from './components/Objects/overlay';
 
-function App() {
-  return (
-    <div className="App">
-      <Overlay img="img/calendar1.png"/>
-      <Nav />
-      <Section />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      projects: true,
+      certifications: true,
+      experience: false,
+      education: false,
+      showOverlay: false,
+      overlayImg: "img/calendar1.png"
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        { this.state.showOverlay ? <Overlay img={this.state.overlayImg}/> : null}
+        <Nav />
+        <Section
+          projects={this.state.projects}
+          certifications={this.state.certifications}
+          experience={this.state.experience}
+          education={this.state.education}/>
+      </div>
+    );
+  }
 }
 
 export default App;
